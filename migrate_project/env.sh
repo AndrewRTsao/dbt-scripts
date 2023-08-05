3b0e666a947ae7fc376d634f6e220a84a35ff122
Andrew Tsao <andrew.tsao@dbtlabs.com>
2023-08-04 23:57:35 -0400

    Adding license update script with some clean up

--- migrate_project/env.sh
+++ migrate_project/env.sh
@@ -1,6 +1,6 @@
 # Information about your dbt Cloud instance needed for script to run
-export BASE_URL="https://staging.singletenant.getdbt.com" # Base URL for your dbt Cloud instance, e.g. https://cloud.getdbt.com
-export ACCOUNT_ID="120" # Account ID for your account. Check this by opening Account Settings and looking in URL
-export SOURCE_PROJECT_ID="390" # Project ID of the source project (check the URL)
-export TARGET_PROJECT_ID="398" # Project ID of the target project (check the URL)
-export API_KEY="bf3b5b8484346c57ae5a67d2887c2683a092494f" # API key / User token / Service token (to make API requests)
+export BASE_URL="" # Base URL for your dbt Cloud instance, e.g. https://cloud.getdbt.com
+export ACCOUNT_ID="" # Account ID for your account. Check this by opening Account Settings and looking in URL
+export SOURCE_PROJECT_ID="" # Project ID of the source project (check the URL)
+export TARGET_PROJECT_ID="" # Project ID of the target project (check the URL)
+export API_KEY="" # API key / User token / Service token (to make API requests)
